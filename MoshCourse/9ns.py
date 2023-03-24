01deeb2bb4df7b7c07e957670033a797a61a3637
fatmeme jadidi <fjadidi2001@gmail.com>
2023-03-24 02:51:00 -0400

    added 9ns

---
+++ MoshCourse/9ns.py
@@ -0,0 +1,18 @@
+command = ""
+while True:
+    command = input(">").lower()
+    if command == "start":
+        print("car started...")
+    elif command == "stop":
+        print("car stopped.")
+    elif command == "help":
+
+        print('''
+start - to start the car
+stop - to stop the car
+quit - to exit
+        ''')
+    elif command == "quit":
+        break
+    else:
+        print("sorry, i dont understand that ")
