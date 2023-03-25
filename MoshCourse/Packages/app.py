9c646019a78ade149e4cdce4c9e492fb4c3af362
fatmeme jadidi <fjadidi2001@gmail.com>
2023-03-25 02:51:46 -0400

    added random

---
+++ MoshCourse/Packages/app.py
@@ -0,0 +1,7 @@
+# tree ways use package
+import eco.shipping
+eco.shipping.calc()
+from eco import shipping
+shipping.calc()
+from eco.shipping import calc
+calc()
---
+++ MoshCourse/Random/01r.py
@@ -0,0 +1,5 @@
+import random
+
+for i in range(5):
+    print(random.random())
+    print(random.randint(10, 100))
