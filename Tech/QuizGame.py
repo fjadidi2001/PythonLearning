69b92436ffce67d41ae44e950d184be1fca99bf7
fatmeme jadidi <fjadidi2001@gmail.com>
2023-03-27 23:12:59 -0400

    second commit on QuizGame

--- Tech/QuizGame.py
+++ Tech/QuizGame.py
@@ -1,8 +1,35 @@
 print("welcome")
 
-playing = input("Name: ")
+name = input("Name: ")
 playing = input("Do u wanna play: ")
 if playing.lower() != "yes":
     quit()
 
 print('Lets Go')
+score = 0
+answer = input("List out components of a computer system. ")
+if answer.lower() == "cpu and memory and input and output device":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect")
+answer = input("List out some popular operating system. ")
+if answer.lower() == "windows and linux and osx":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect")
+answer = input("List out some computer processors. ")
+if answer.lower() == "intel and amd":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect")
+answer = input("What is the full form of SDLC?. ")
+if answer.lower() == "software development life cycle":
+    print('correct!')
+    score += 1
+else:
+    print("incorrect")
+print(name + ' got ' + str(score) + " questions correct!")
+print(name+" got " + str((score/4)*100) + "%.")
