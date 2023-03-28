af251567d19b42103fdf4489e27d4a9ae882707f
fatmeme jadidi <fjadidi2001@gmail.com>
2023-03-27 23:48:48 -0400

    added new ex to set

--- w3shool/type/set.py
+++ w3shool/type/set.py
@@ -6,3 +6,15 @@ print(seti1)
 seti2 = {"one", "two", "three", False, 0}
 print(seti2, len(seti2))
 seti3 = set(("hi", "hello", "cherry"))
+for i in seti3:
+    print(i)
+# check item in a list
+print("hi" in seti3)
+seti3.add('but')
+for i in seti3:
+    print(i)
+seti3.update(seti2)
+print(seti3)
+listi = ["ha", "joker"]
+seti2.update(listi)
+print(seti2)
