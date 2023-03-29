00500ff0b43f0eb51a6daff7e044bd03ff9c2e4b
fatmeme jadidi <fjadidi2001@gmail.com>
2023-03-29 17:54:20 -0400

    added md file

--- Tech/NumberGuessingGame.py
+++ Tech/NumberGuessingGame.py
@@ -1,1 +1,12 @@
 import random
+
+topOfRange = input("Type of number: ")
+if topOfRange.isdigit():
+    topOfRange = int(topOfRange)
+    if topOfRange <= 0:
+        print('Enter number>0')
+        quit()
+else:
+    print("print a number")
+randomNumber = random.randint(0, topOfRange)
+print(randomNumber)
---
+++ Tech/readme.md
@@ -0,0 +1,5 @@
+# random function
+randint(start, stop)<br>
+randint include stop<br>
+randrange(start, stop)<br>
+randrange doesnt include stop number<br>
