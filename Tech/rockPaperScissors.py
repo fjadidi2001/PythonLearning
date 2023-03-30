f140c8f76d883c143830e020e78d6291fbaf9348
fatmeme jadidi <fjadidi2001@gmail.com>
2023-03-29 22:32:44 -0400

    init rock

---
+++ Tech/rockPaperScissors.py
@@ -0,0 +1,33 @@
+import random
+
+userWins = 0
+computerWins = 0
+options = ["rock", "paper", "scissors"]
+while True:
+    user_input = input("Type R/P/S or Q to quit: ").lower()
+    if user_input == "q":
+        break
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+    computer_pick = options[random_number]
+    print("computer picked", computer_pick)
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("you won")
+        userWins += 1
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("you won")
+        userWins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("you won")
+        userWins += 1
+    else:
+        print("you lost")
+        computerWins += 1
+print("you won", userWins, "times")
+print("the computer won", computerWins, "times")
+print("Game over")
