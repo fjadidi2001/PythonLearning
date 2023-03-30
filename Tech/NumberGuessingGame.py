671a47289ce116cf8ac450756593ef8c0ca27503
fatmeme jadidi <fjadidi2001@gmail.com>
2023-03-29 22:07:30 -0400

    added number

--- Tech/NumberGuessingGame.py
+++ Tech/NumberGuessingGame.py
@@ -1,12 +1,29 @@
 import random
 
-topOfRange = input("Type of number: ")
+topOfRange = input("range: ")
 if topOfRange.isdigit():
     topOfRange = int(topOfRange)
     if topOfRange <= 0:
         print('Enter number>0')
         quit()
 else:
-    print("print a number")
+    print("print integer number")
+    quit()
 randomNumber = random.randint(0, topOfRange)
-print(randomNumber)
+guesses = 0
+while True:
+    guesses += 1
+    user_guess = input("make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('enter another guess: ')
+        continue
+    if user_guess == randomNumber:
+        print("you got it")
+        break
+    elif user_guess > randomNumber:
+        print("you were above the number:)")
+    else:
+        print("you were below the number:)")
+print("you got it in" + guesses + "guesses")
