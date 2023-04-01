9d01aa452fa4042f8d1b07a103d3e594b686f85b
fatmeme jadidi <fjadidi2001@gmail.com>
2023-03-31 23:58:01 -0400

    added third answer

--- Training/str.py
+++ Training/str.py
@@ -1,1 +1,5 @@
 # Exercise 7: Return the count of a given substring from a string
+str_x = "Emma is good developer. Emma is a writer"
+# use count method of a str class
+cnt = str_x.count("Emma")
+print(cnt)
