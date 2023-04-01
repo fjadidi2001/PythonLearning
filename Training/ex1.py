a1b029ec65ebe158b285f86da411cbdb82ce0043
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-01 02:07:50 -0400

    added second solution

--- Training/ex1.py
+++ Training/ex1.py
@@ -1,8 +1,21 @@
 # Exercise 1: Calculate the multiplication and sum of two numbers
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-result = num1 * num2
-if result < 1000:
-    print(f"the result is: {result}")
-else:
-    print(f"the result is: {num1 + num2}")
+# first solution:
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# result = num1 * num2
+# if result < 1000:
+#     print(f"the result is: {result}")
+# else:
+#     print(f"the result is: {num1 + num2}")
+
+# second solution:
+def multiplicationOrSum(num1, num2):
+    result = num1 * num2
+    if result <= 1000:
+        print(f"the result is: {result}")
+    else:
+        print(f"the result is: {num1 + num2}")
+
+
+multiplicationOrSum(40, 30)
+multiplicationOrSum(20, 30)
