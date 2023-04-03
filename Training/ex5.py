13042155885ac6c57ed7f057308432c193f244da
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-03 00:10:42 -0400

    added solution for ex5

--- Training/ex5.py
+++ Training/ex5.py
@@ -1,1 +1,18 @@
 # Exercise 5: Check if the first and last number of a list is the same
+n = int(input("Enter number of elements: "))
+a = list(map(int,
+             input("\nEnter the numbers : ").strip().split()))[:n]
+
+print("\nGiven list: ", a)
+
+
+def Check_list(a):
+    first = a[0]
+    last = a[-1]
+    if first == last:
+        return True
+    else:
+        return False
+
+
+print("result is", Check_list(a))
