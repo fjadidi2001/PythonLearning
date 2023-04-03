9842e87291b23f613e74b1730fe637b1baa8c633
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-02 21:27:50 -0400

    first solution

---
+++ Training/ex3.py
@@ -0,0 +1,6 @@
+# Exercise 3: Print characters from a string that are present at an even index number
+olm = str(input("Enter your string: "))
+cal = len(olm)
+for i in range(0, cal - 1, 2):
+    print(olm[i])
+
