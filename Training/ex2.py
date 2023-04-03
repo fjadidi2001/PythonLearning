2c3e1d2b64e749c6e2c97e6468b0c546530a0542
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-02 21:00:58 -0400

    int input

--- Training/ex2.py
+++ Training/ex2.py
@@ -1,24 +1,21 @@
 # Exercise 2: Print the sum of the current number and the previous number
 
 
-# def get_amount():
-#     while True:
-#         amount = input("Enter amount: ")
-#         try:
-#             val = int(amount)
-#             if val >= 0:
-#                 break
-#             else:
-#                 print("Amount can't be negative, try again")
-#         except ValueError:
-#             print("Amount must be a number, try again")
-#     return val
-#
-#
-# get_amount()
+def get_amount():
+    while True:
+        amount = input("Enter amount: ")
+        try:
+            val = int(amount)
+            if val >= 0:
+                break
+            else:
+                print("Amount can't be negative, try again")
+        except ValueError:
+            print("Amount must be a number, try again")
+    return val
 
 
-for i in range(10):
+for i in range(get_amount()):
     current = i
 
     if current == 0:
