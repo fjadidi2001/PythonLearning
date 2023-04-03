d5376bcb6b282032292d42646f8175a8a8162ea6
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-02 23:36:14 -0400

    added first solution

--- Training/ex4.py
+++ Training/ex4.py
@@ -1,2 +1,8 @@
 # Exercise 4: Remove first n characters from a string
 
+word = str(input("Enter you world: "))
+remove_chars = int(input("Enter a number: "))
+for i in range(len(word)):
+    var = word[remove_chars:]
+    print(var)
+    break
