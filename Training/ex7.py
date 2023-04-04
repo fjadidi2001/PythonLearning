3fa750e8e3eb994140a2dbb5c2097d912ba3dc6c
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-03 23:52:19 -0400

    first try

--- Training/ex7.py
+++ Training/ex7.py
@@ -1,1 +1,8 @@
 # Exercise 7: Return the count of a given substring from a string
+str_x = str(input("Enter a sentence: "))
+str_y = str(input("Enter substring: "))
+for i in str_x:
+    count = 0
+    if str_y in str_x:
+        count += 1
+    print(count)
