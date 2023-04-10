adc3ffa7c579ca6645733fdc1a524320f4cf2828
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-09 23:58:59 -0400

    added ex9

--- Training/ex9.py
+++ Training/ex9.py
@@ -1,1 +1,16 @@
-# Exercise 9: Check Palindrome Number
+# Exercise 9: Check Palindrome string
+
+s = input('enter string: ')
+
+
+def palindrome(string):
+    x = ""
+    for i in string:
+        x = i + x
+    return x
+
+
+if s == palindrome(s):
+    print('its a palindrome')
+else:
+    print('its not a palindrome')
