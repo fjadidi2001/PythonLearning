a543c32ea2a1d99a6eb6d30d788257da3866b5fa
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-10 22:58:57 -0400

    ver4 with bug of pass

--- Tech/PasswordManager.py
+++ Tech/PasswordManager.py
@@ -1,8 +1,5 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input("what is the master password? ")
-
-
 # def write_key():
 #     key = Fernet.generate_key()
 #     with open("key.key", "wb") as key_file:
@@ -11,19 +8,32 @@ master_pwd = input("what is the master password? ")
 #
 
 
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+master_pwd = input("what is the master password? ")
+key = load_key() + master_pwd.bytes
+fer = Fernet(key)
+
+
+
+
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:", user, ",Password:", passw)
+            print("User:", user, ",Password:", +str(fer.encrypt(passw.encode())))
 
 
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + +str(fer.encrypt(pwd.encode())) + "\n")
 
 
 while True:
