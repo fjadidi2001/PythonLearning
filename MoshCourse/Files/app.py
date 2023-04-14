0ebe27af07fa55d8dfad8639014c6574f18ab1b0
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-13 23:35:29 -0400

    added glob it find all py file

--- MoshCourse/Files/app.py
+++ MoshCourse/Files/app.py
@@ -1,7 +1,12 @@
 from pathlib import Path
+
 path = Path('eco')
 print(path.exists())
 path = Path('emails')
 print(path.exists())
-path = Path('emails')
-print(path.rmdir())
+# path = Path('emails')
+# print(path.mkdir())
+# print(path.rmdir())
+path = Path()
+# return all py file in this directory
+print(path.glob('*.py'))
