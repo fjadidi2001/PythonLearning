6c011640d87fefcbed10504f7290fb3289688837
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-19 20:51:00 -0400

    added models

--- MoshCourse/Django/product/models.py
+++ MoshCourse/Django/product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.CharField()
+    stock = models.IntegerField()
+    image_urls = models.CharField(max_length=2083)
+
+
+
