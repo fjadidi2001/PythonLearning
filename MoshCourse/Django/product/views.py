efe8775c4df5fb2712ef8808a9782478e53d6e5a
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-23 21:33:51 -0400

    added view

--- MoshCourse/Django/product/views.py
+++ MoshCourse/Django/product/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 def index(request):
+    products = Product.objects.all()
     return HttpResponse("this is is")
 
 
