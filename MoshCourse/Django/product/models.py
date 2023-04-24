c5ae3b3db8a93e6c4c4ca8bf65a892c3ba4745e9
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-23 21:26:22 -0400

    fix models

--- MoshCourse/Django/product/models.py
+++ MoshCourse/Django/product/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.CharField(max_length=225)
-    stock = models.IntegerField(max_length=2083)
+    stock = models.IntegerField()
     image_urls = models.CharField(max_length=2083)
 
 
