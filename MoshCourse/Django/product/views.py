c386ce84ed548c5852123d0181a1b32a8fe2cac1
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-23 21:41:13 -0400

    added models to index-change view

--- MoshCourse/Django/product/views.py
+++ MoshCourse/Django/product/views.py
@@ -5,7 +5,8 @@ from .models import Product
 
 def index(request):
     products = Product.objects.all()
-    return render(request, 'index.html')
+    return render(request, 'index.html',
+                  {'products': products})
 
 
 def new(request):
