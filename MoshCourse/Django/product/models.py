a00d5cf415aeac1ca101ce47909131a8ecdfff61
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-23 20:59:05 -0400

    added new model

--- MoshCourse/Django/product/models.py
+++ MoshCourse/Django/product/models.py
@@ -1,11 +1,14 @@
-# from django.db import models
-#
-#
-# class Product(models.Model):
-#     name = models.CharField(max_length=255)
-#     price = models.CharField(max_length=225)
-#     stock = models.IntegerField(max_length=2083)
-#     image_urls = models.CharField(max_length=2083)
-#
-#
-#
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.CharField(max_length=225)
+    stock = models.IntegerField(max_length=2083)
+    image_urls = models.CharField(max_length=2083)
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=255)
+    des = models.IntegerField(max_length=2083)
+    dis = models.IntegerField(max_length=2083)
