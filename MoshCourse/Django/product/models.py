6a37a6a70b361efef9695f4fc1c697f42f112ddd
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-23 20:53:20 -0400

    added migration

--- MoshCourse/Django/product/models.py
+++ MoshCourse/Django/product/models.py
@@ -1,11 +1,11 @@
-from django.db import models
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.CharField(max_length=225)
-    stock = models.IntegerField(max_length=2083)
-    image_urls = models.CharField(max_length=2083)
-
-
-
+# from django.db import models
+#
+#
+# class Product(models.Model):
+#     name = models.CharField(max_length=255)
+#     price = models.CharField(max_length=225)
+#     stock = models.IntegerField(max_length=2083)
+#     image_urls = models.CharField(max_length=2083)
+#
+#
+#
