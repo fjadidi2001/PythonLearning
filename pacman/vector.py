faf3b2f3ff885aa937c7572c353f47f11e2cb7b6
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-26 23:58:18 -0400

    added equal def

--- pacman/vector.py
+++ pacman/vector.py
@@ -27,3 +27,10 @@ class Vector2(object):
 
 def __truediv__(self, scalar):
     return self.__div__(scalar)
+
+
+def __eq__(self, other):
+    if abs(self.x - other.x) < self.thresh:
+        if abs(self.y - other.y) < self.thresh:
+            return True
+    return False
