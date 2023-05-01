4a4648528a67bb917bcfdd4e8db588850e7ea482
fatmeme jadidi <fjadidi2001@gmail.com>
2023-04-30 23:58:20 -0400

    first try

--- Training/I-O/AcceptNumFromUser.py
+++ Training/I-O/AcceptNumFromUser.py
@@ -1,1 +1,7 @@
 # Write a program to accept two numbers from the user and calculate multiplication
+num1 = int(input("Enter number 1:"))
+num2 = int(input("Enter number 2:"))
+
+
+def Multiplication():
+    return num1*num2
