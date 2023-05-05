4be1abd682f57e7d7fa69dbdc3b1e6ae0802c702
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-05 15:53:48 -0400

    added another solution to convert

--- Training/I-O/convert.py
+++ Training/I-O/convert.py
@@ -1,3 +1,6 @@
 # Convert Decimal number to octal using print() output formatting
 num = int(input('Enter a number: '))
 print(f'this is Decimal number: {num}', 'and convert this number to octal is: %o' % num)
+# another solution
+Decimal = oct(num)
+print(f'this is Decimal number: {num}', f'and convert this number to octal is: {Decimal}')
