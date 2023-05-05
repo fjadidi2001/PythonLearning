fe6eb0ecd795b1bbc7f6e3c88e622bedb934732f
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-05 16:55:47 -0400

    fix content3 for example

--- Training/I-O/content3.py
+++ Training/I-O/content3.py
@@ -1,8 +1,8 @@
 numbers = []
-with open('text.txt', 'r') as fp:
-    li = fp.readline()
-with open('text.txt', 'w') as fp:
+
+with open('text.txt', 'a+', encoding='utf-8') as f:
     for i in range(0, 5):
         num = float(input('Enter a number: '))
         numbers.append(num)
+    f.write(str(numbers))
 
