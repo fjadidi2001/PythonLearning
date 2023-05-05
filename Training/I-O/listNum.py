ccd62cbb254a6eab78f96faff31d85a4c68808ad
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-05 16:19:28 -0400

    added solution listNum

---
+++ Training/I-O/listNum.py
@@ -0,0 +1,7 @@
+# Exercise 5: Accept a list of 5 float numbers as an input from the user
+number = []
+for i in range(0, 5):
+    num = float(input('Enter float number: '))
+    number.append(num)
+print('user list: ', number)
+
