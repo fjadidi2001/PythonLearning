a222b8411ef675f2f7955d63878af1745e33dc99
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-05 16:20:31 -0400

    added another solution listNum

--- Training/I-O/listNum.py
+++ Training/I-O/listNum.py
@@ -4,4 +4,12 @@ for i in range(0, 5):
     num = float(input('Enter float number: '))
     number.append(num)
 print('user list: ', number)
+# another solution
+for i in range(0, 5):
+    print("Enter number at location", i, ":")
+    # accept float number from user
+    item = float(input())
+    # add it to the list
+    number.append(item)
 
+print("User List:", number)
