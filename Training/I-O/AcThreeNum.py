608d35c8f75ad47edfc157b789f55e9fe018efc2
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-07 11:14:49 -0400

    fix some issues

--- Training/I-O/AcThreeNum.py
+++ Training/I-O/AcThreeNum.py
@@ -1,4 +1,9 @@
 # Exercise 7: Accept any three string from one input() call
 for i in range(0, 3):
-    num = str.split(input('Enter 3 name: '))
+    num = str.split(input('Enter a name: '))
 
+# solution two
+# str1, str2, str3 = input("Enter three string").split()
+# print('Name1:', str1)
+# print('Name2:', str2)
+# print('Name3:', str3)
