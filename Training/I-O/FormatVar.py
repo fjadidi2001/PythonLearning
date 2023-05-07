a563261af36cf0843bee188e5b766cfbfbfb0ce8
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-07 11:27:40 -0400

    added first solution

---
+++ Training/I-O/FormatVar.py
@@ -0,0 +1,4 @@
+totalMoney = int(input('Enter your total money: '))
+quantity = int(input('How many do you want to buy thing?: '))
+price = int(input('Enter the price:'))
+print(f'I have {totalMoney}$ dollars so I can buy {quantity} football for {price}$ dollars.')
