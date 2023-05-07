ea768370e0ca81a8ebffe934c71b8cc7ab167aa2
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-07 11:31:05 -0400

    added solution to Format

--- Training/I-O/FormatVar.py
+++ Training/I-O/FormatVar.py
@@ -2,3 +2,10 @@ totalMoney = int(input('Enter your total money: '))
 quantity = int(input('How many do you want to buy thing?: '))
 price = int(input('Enter the price:'))
 print(f'I have {totalMoney}$ dollars so I can buy {quantity} football for {price}$ dollars.')
+
+# SOLUTION TWO
+quantity = 3
+totalMoney = 1000
+price = 450
+statement1 = "I have {1} dollars so I can buy {0} football for {2:.2f} dollars."
+print(statement1.format(quantity, totalMoney, price))
