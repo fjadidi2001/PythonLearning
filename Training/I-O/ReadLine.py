dae56f1ee88ceef6a7b2e87b7f1789625a6351a0
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-13 23:44:15 -0400

    added solution for readline

--- Training/I-O/ReadLine.py
+++ Training/I-O/ReadLine.py
@@ -1,12 +1,7 @@
 # Exercise 10: Read line number 4 from the following file
-from itertools import islice
 
-# print the 15th line
-with open('new_file.txt') as lines:
-    for line in islice(lines, 14, 15):
-        print(line)
+# print the 4th line
+with open('new_file.txt','r') as fp:
+    x = fp.readlines()
+    print(x[3])
 
-# print each third line until 25
-with open('new_file.txt') as lines:
-    for line in islice(lines, 0, 25, 3):
-        print(line)
