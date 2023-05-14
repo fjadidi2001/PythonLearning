45ab15593df520b92051934588f8c8947096cacc
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-13 23:27:05 -0400

    added another solution for checking file

--- Training/I-O/check.py
+++ Training/I-O/check.py
@@ -1,15 +1,23 @@
 # Exercise 9: Check file is empty or not
-with open('test.txt', 'r') as fp:
-    empty = fp.readlines()
-    for cou in empty:
-        if cou == 0:
-            print('file is empty')
-            break
-    print('file is not empty')
+# with open('test.txt', 'r') as fp:
+#     empty = fp.readlines()
+#     for cou in empty:
+#         if cou == 0:
+#             print('file is empty')
+#             break
+#     print('file is not empty')
 
-# this is another solution and it is not work
+# another solution
+# import os
+#
+# size = os.stat("text.txt").st_size
+# if size == 0:
+#     print('file is empty')
+# another solution using try and except
+
 import os
-
-size = os.stat("text.txt").st_size
-if size == 0:
-    print('file is empty')
+try:
+    if os.stat("new_file.txt").st_size == 0:
+        print('file is empty')
+except FileNotFoundError:
+    print('the specified file does Not exist')
