4567e77b2e521b77dfd09b7c266406b4d6284640
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-13 23:48:55 -0400

    this is not the ansewer but it could be helpful

--- Training/I-O/ReadLine.py
+++ Training/I-O/ReadLine.py
@@ -1,7 +1,13 @@
 # Exercise 10: Read line number 4 from the following file
 
 # print the 4th line
-with open('new_file.txt','r') as fp:
-    x = fp.readlines()
-    print(x[3])
+# with open('new_file.txt', 'r') as fp:
+#     x = fp.readlines()
+#     print(x[3])
 
+filePath = "test.txt"
+openMode = "r"
+
+with open(filePath, openMode) as f:
+    for i, line in enumerate(f):
+        print(i, line)
