c67f6b08402a2ac6fb1433cbf9e184c4c476cfb4
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-13 23:31:58 -0400

    added another solution for checking file

--- Training/I-O/check.py
+++ Training/I-O/check.py
@@ -7,17 +7,30 @@
 #             break
 #     print('file is not empty')
 
+
 # another solution
 # import os
 #
 # size = os.stat("text.txt").st_size
 # if size == 0:
 #     print('file is empty')
+
+
 # another solution using try and except
 
+# import os
+#
+# try:
+#     if os.stat("new_file.txt").st_size == 0:
+#         print('file is empty')
+# except FileNotFoundError:
+#     print('the specified file does Not exist')
+
+
+# another solution
+
 import os
-try:
-    if os.stat("new_file.txt").st_size == 0:
-        print('file is empty')
-except FileNotFoundError:
-    print('the specified file does Not exist')
+if os.path.getsize('text.txt')==0:
+    print('file is empty')
+else:
+    print('file is not empty')
