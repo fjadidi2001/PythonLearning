eccda68e2945f8ca6fb7c088bfb3aa537aa43123
fjadidi2001 <fjadidi2001@gmail.com>
2023-05-15 23:29:20 -0400

    added another solution

--- Training/loop/calculate.py
+++ Training/loop/calculate.py
@@ -1,6 +1,11 @@
 # Exercise 3: Calculate the sum of all numbers from 1 to a given number
-num = int(input("Enter a number: "))
-s = 0
-for i in range(1, num+1):
-    s += i
-print(s)
+# num = int(input("Enter a number: "))
+# s = 0
+# for i in range(1, num+1):
+#     s += i
+# print(s)
+#
+n = int(input("Enter number "))
+# pass range of numbers to sum() function
+x = sum(range(1, n + 1))
+print('Sum is:', x)
