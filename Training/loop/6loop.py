6cbb9c74655fd72d80785c62bf9e865d67e3079e
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-06-08 16:30:12 -0400

    Update 6loop.py

--- Training/loop/6loop.py
+++ Training/loop/6loop.py
@@ -1,5 +1,8 @@
 
 # Exercise 5: Display numbers from a list using loop
+#  The number must be divisible by five
+#  If the number is greater than 150, then skip it and move to the next number
+#  If the number is greater than 500, then stop the loop
 numbers = [12, 75, 150, 180, 145, 525, 50]
 # iterate each item of a list
 for item in numbers:
