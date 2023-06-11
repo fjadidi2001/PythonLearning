bf25bcdb36b3e32c125fc16664943e69b8b20504
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-06-11 16:34:26 -0400

    Update Ex6Count.py

--- Training/loop/Ex6Count.py
+++ Training/loop/Ex6Count.py
@@ -1,10 +1,8 @@
 
 # Exercise 6: Count the total number of digits in a number
-num = 75869
+num = int(input("Enter your num: "))
 count = 0
 while num != 0:
-
     num = num // 10
-
     count = count + 1
 print("Total digits are:", count)
