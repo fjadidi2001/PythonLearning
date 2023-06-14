c998ca51f7bca5c37a6556712b24bdafdc78c38a
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-06-14 16:19:55 -0400

    added sol2 Ex8Print.py

--- Training/loop/Ex8Print.py
+++ Training/loop/Ex8Print.py
@@ -8,3 +8,12 @@ for list1 in new_list:
     print(item)
 
 
+#solution2
+list1 = [10, 20, 30, 40, 50]
+# get list size
+# len(list1) -1: because index start with 0
+# iterate list in reverse order
+# star from last item to first
+size = len(list1) - 1
+for i in range(size, -1, -1):
+    print(list1[i])
