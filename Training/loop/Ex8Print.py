a98d7215bdf88434e4d23a8d55713c685ff38b17
fjadidi2001 <96652895+fjadidi2001@users.noreply.github.com>
2023-06-14 16:19:31 -0400

    sol1 Ex8Print.py

--- Training/loop/Ex8Print.py
+++ Training/loop/Ex8Print.py
@@ -1,2 +1,10 @@
 # Exercise 8: Print list in reverse order using a loop
+#solution1
+list1 = [10, 20, 30, 40, 50]
+# reverse list
+new_list = reversed(list1)
+# iterate reversed list
+for list1 in new_list:
+    print(item)
+
 
